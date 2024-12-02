af46cdeec0cc0a2cb72a00ab523652082e501e13
Mark Allen <ma230@sbcglobal.net>
2024-12-02 09:51:55 -0600

    3

--- src/main/java/net/ma230/tutorialmod/TutorialMod.java
+++ src/main/java/net/ma230/tutorialmod/TutorialMod.java
@@ -22,7 +22,7 @@ import org.slf4j.Logger;
 public class TutorialMod
 {
     public static final String MOD_ID = "tutorialmod";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public TutorialMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
